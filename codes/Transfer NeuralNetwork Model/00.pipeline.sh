3f0b329df23b6a8f152355b1f1213de0dd1fc188
wangnan990826 <61443247+wangnan990826@users.noreply.github.com>
2022-10-22 21:56:05 +0800

    Add files via upload

---
+++ codes/Transfer NeuralNetwork Model/00.pipeline.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+/bin/bash 01.ontology_construction.sh
+/bin/bash 02.source_mapping.sh
+/bin/bash 03.data_convert.sh
+/bin/bash 04.transfer.sh
+/bin/bash 05.source_tracking.sh
+/bin/bash 06.evaluation.sh
+
